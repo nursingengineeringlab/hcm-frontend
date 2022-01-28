e757cc3a6ff177b2753e365fa7ee85ab77f13738
Shiyang Wang <shiyangwang@umass.edu>
2022-01-28 10:14:08 -0500

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,  
   document.getElementById('root')
 );
 
