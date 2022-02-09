75ae249e01dff88061a93a2eb5e861b84e4ca765
Shiyang Wang <shiyangwang@umass.edu>
2022-02-09 18:11:46 -0500

    update

--- src/components/Dashboard/DataGraph.js
+++ src/components/Dashboard/DataGraph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ResponsiveLine } from '@nivo/line'
 import {Device_Description} from './DeviceType.js'
+import {http_public_url, api_port, dashboardHeaders} from "../../config.js"
 
 const CustomSymbol = ({ size, color, borderWidth, borderColor }) => (
     <g>
@@ -16,6 +17,30 @@ const CustomSymbol = ({ size, color, borderWidth, borderColor }) => (
 )
 
 export class MyGraph extends React.Component {
+    componentDidMount(){
+        var api_base_url = http_public_url + ":" + api_port + "/";
+
+        fetch(api_base_url + 'sensordata/RRI', {
+            method: 'GET',
+            headers: dashboardHeaders
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        //   for (var element of data['results']) {
+        //     element["data"] = [{"value": 60, "time": 0}];
+        //     element["battery"] = 60;
+        //     element["watch"] = exceeded_threshold(element.data[element.data.length - 1].value, element['device_type']);  // determine whether to add to watch list
+        //     element["active"] = false;
+        //     element["color"] = randomColor({luminosity: 'dark',});
+        //     this.OnlineSeniors.set(element['device_id'], element);
+        //   }
+        //   this.setState({flag: !this.state.flag});  // Triggers a re-rendering
+        }).catch(err => {
+          console.log(err)
+        });
+    }
+
     render(){
         let ndata = [];
         for (var i=0; i<this.props.data.data.length; i++){
