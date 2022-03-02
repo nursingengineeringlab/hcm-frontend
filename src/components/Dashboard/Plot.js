81b9a9972d3cb318548851102715e65e9b805c07
Shiyang Wang <shiyangwang@umass.edu>
2022-03-02 15:35:25 -0500

    make test data

--- src/components/Dashboard/Plot.js
+++ src/components/Dashboard/Plot.js
@@ -7,43 +7,46 @@ export class Plott extends React.Component {
         this.kind = props.kind;
     }
     state = {
+        timer: new Date().getTime(),
         line1: {
-          x: [-3, -2, -1],
-          y: [1, 2, 3], 
-          name: 'Line 1'
+          x: [],
+          y: [], 
+          name: 'ECG'
         },
-        line2: {
-          x: [1, 2, 3],
-          y: [-3, -2, -1],
-          name: 'Line 2'
-        }, 
         layout: { 
           datarevision: 0,
           title: this.kind,
           xaxis: {
             autorange: true,
-            range: ['2015-02-17', '2017-02-16'],
+            range: [0, 86400],
+            // https://plotly.com/python/reference/layout/xaxis/#layout-xaxis-rangeselector   
             rangeselector: {buttons: [
                 {
                   count: 1,
-                  label: '1m',
-                  step: 'month',
+                  label: '1s',
+                  step: 'second',
                   stepmode: 'backward'
                 },
                 {
-                  count: 6,
-                  label: '6m',
-                  step: 'month',
+                  count: 30,
+                  label: '30m',
+                  step: 'minute',
+                  stepmode: 'backward'
+                },
+                {
+                  count: 1,
+                  label: '1h',
+                  step: 'hour',
                   stepmode: 'backward'
                 },
                 {step: 'all'}
               ]},
-            rangeslider: {range: ['2015-02-17', '2017-02-16']},
+            rangeslider: {autorange: true},
             type: 'date'
           },
           yaxis: {
             autorange: true,
-            range: [86.8700008333, 138.870004167],
+            range: [0, 1500],
             type: 'linear'
           }
         },
@@ -52,21 +55,33 @@ export class Plott extends React.Component {
     componentDidMount() {
         setInterval(this.increaseGraphic, 1000);
     } 
-    rand = () => parseInt(Math.random() * 10 + this.state.revision, 10);
+
+    rand = () => parseInt(Math.random() * 100 + this.state.revision, 10);
     
+//         let data_array = this.kind === "RRI" ? this.props.data.rri_data : this.props.data.temp_data;
+//         console.log(this.kind);
+
+//         for (var i=0; i < data_array.length; i++){
+//             counter++;
+//             dataPoints.push({	
+// 				x: counter,
+// 				y: data_array[i].value
+// 			});
+//         }
+
     increaseGraphic = () => {
-        const { line1, line2, layout } = this.state;
-        line1.x.push(this.rand());
-        line1.y.push(this.rand());
-        if (line1.x.length >= 10) {
+        const { line1, layout } = this.state;
+        console.log(this.state.timer);
+        this.state.timer += 1000;
+        line1.x.push(this.state.timer);
+        if(this.kind == "RRI") {
+          line1.y.push(this.rand());
+        } else {
+          line1.y.push(this.rand()+100);
+        }
+        if (line1.x.length >= 1000) {
           line1.x.shift();
           line1.y.shift();
-        } 
-        line2.x.push(this.rand());
-        line2.y.push(this.rand());
-        if (line2.x.length >= 10) {
-          line2.x.shift();
-          line2.y.shift();
         }
         this.setState({ revision: this.state.revision + 1 });
         layout.datarevision = this.state.revision + 1;
@@ -74,30 +89,14 @@ export class Plott extends React.Component {
     
 
     render() {
-        var trace1 = {
-            type: "scatter",
-            mode: "lines",
-            name: 'AAPL High',
-            x: [1,2,3,4,5],
-            y: [7,1,3,1,213],
-            line: {color: '#17BECF'}
-        }
-                    
-        var data = [trace1];
-         
-        var layout = {
-          
-        };
 
         return (
         <Plot
             data={[
                 this.state.line1,
-                this.state.line2,
               ]}
             layout={this.state.layout}
             revision={this.state.revision}
-            // layout={layout }
         />
         );
   }
