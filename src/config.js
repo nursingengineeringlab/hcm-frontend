7837f3309070a564408a7c2c0925329137c698ad
Shiyang Wang <shiyangwang@umass.edu>
2022-03-29 15:33:07 -0400

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,23 +1,12 @@
 module.exports = global.config = {
-<<<<<<< HEAD
-    api_port: "31000",
-    http_public_url: "http://nelab.ddns.umass.edu",
-    mqtt_url: "ws://nelab.ddns.umass.edu:30848/mqtt",
-    data_fetcher_http_url: "http://nelab.ddns.umass.edu",
-    // http_public_url: "http://20.231.78.167",
-    // mqtt_url: "ws://20.231.78.167:8083/mqtt",
-    // data_fetcher_http_url: "http://20.231.78.167",
-    data_fetcher_port: "30000",
-=======
     cluster_ip: "nelab.ddns.umass.edu",
 
     //most of those port does not need to be changed.
     api_port: "31000",
     data_fetcher_port: "30000",
     mqtt_port : "30482",
-    http_public_url: "http://nelab.ddns.umass.edu",
+    http_public_url: "http://nelab.ddns.umass.edu/api",
     mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
     data_fetcher_http_url: "http://" + "nelab.ddns.umass.edu",
->>>>>>> update config url and port
     dashboardHeaders: new Headers()
 };
