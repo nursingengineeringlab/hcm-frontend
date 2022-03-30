87363857e1a1d187da90a87317eb4106d0c484cd
Shiyang Wang <shiyangwang@umass.edu>
2022-03-30 18:11:17 -0400

    change to https

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 module.exports = global.config = {
-    cluster_ip: "nelab.ddns.umass.edu",
 
     //most of those port does not need to be changed.
     data_fetcher_port: "30000",
     mqtt_port : "30482",
-    http_public_url: "http://nelab.ddns.umass.edu/backend/",
+    http_public_url: "https://nelab.ddns.umass.edu/backend/",
     mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
     data_fetcher_http_url: "http://" + "nelab.ddns.umass.edu",
     dashboardHeaders: new Headers()
