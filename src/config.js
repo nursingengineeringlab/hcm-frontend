ea7f9de29076d8ade105004cd6bdd64b906177d9
Shiyang Wang <shiyangwang@umass.edu>
2022-03-30 16:46:20 -0400

    add dash

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = global.config = {
     //most of those port does not need to be changed.
     data_fetcher_port: "30000",
     mqtt_port : "30482",
-    http_public_url: "http://nelab.ddns.umass.edu",
+    http_public_url: "http://nelab.ddns.umass.edu/",
     mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
     data_fetcher_http_url: "http://" + "nelab.ddns.umass.edu",
     dashboardHeaders: new Headers()
