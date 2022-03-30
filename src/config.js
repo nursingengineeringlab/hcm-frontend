c1ddc18e40f9216a9201f8d17501aa41cf359987
Shiyang Wang <shiyangwang@umass.edu>
2022-03-30 16:09:21 -0400

    revert url change back

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = global.config = {
     api_port: "31000",
     data_fetcher_port: "30000",
     mqtt_port : "30482",
-    http_public_url: "http://nelab.ddns.umass.edu/api",
+    http_public_url: "http://nelab.ddns.umass.edu",
     mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
     data_fetcher_http_url: "http://" + "nelab.ddns.umass.edu",
     dashboardHeaders: new Headers()
