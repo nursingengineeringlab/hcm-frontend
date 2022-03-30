5a0f9618e0bbf005021efeefdf65c91259cbdfbe
Shiyang Wang <shiyangwang@umass.edu>
2022-03-30 18:30:00 -0400

    change to wss

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = global.config = {
     data_fetcher_port: "30000",
     mqtt_port : "30482",
     http_public_url: "https://nelab.ddns.umass.edu/backend/",
-    mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
+    mqtt_url: "wss://nelab.ddns.umass.edu" + ":" + "32729" + "/mqtt",
     data_fetcher_http_url: "http://" + "nelab.ddns.umass.edu",
     dashboardHeaders: new Headers()
 };
