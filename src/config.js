3a82e32d4440363a405f93d7bbe4290bde08dfe3
Shiyang Wang <shiyangwang@umass.edu>
2022-03-31 14:23:13 -0400

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
-    mqtt_port : "30482",
     http_public_url: "https://nelab.ddns.umass.edu/backend/",
     mqtt_url: "wss://nelab.ddns.umass.edu" + ":" + "32729" + "/mqtt",
     data_fetcher_http_url: "https://" + "nelab.ddns.umass.edu/",
