849b1bb48dd3156ee64fefb855c166bb013cb690
Shiyang Wang <shiyangwang@umass.edu>
2022-04-12 14:52:30 -0400

    fall back to http

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
-<<<<<<< HEAD
     http_public_url: "https://nelab-ingress.eastus.cloudapp.azure.com/backend/",
     mqtt_url: "wss://nelab-ingress.eastus.cloudapp.azure.com/" + ":" + "32729" + "/mqtt",
     data_fetcher_http_url: "https://nelab-ingress.eastus.cloudapp.azure.com/",
-=======
-    http_public_url: "https://nelab.ddns.umass.edu/backend/",
-    mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848" + "/mqtt",
-    data_fetcher_http_url: "https://" + "nelab.ddns.umass.edu/",
->>>>>>> revert back to ws
     dashboardHeaders: new Headers()
 };
