9611caa636ba2affcc93f21818f3afc15db6f6e7
Shiyang Wang <shiyangwang@umass.edu>
2022-04-18 21:28:37 -0400

    update remove ep /api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
-    http_public_url: "https://nelab-ingress.eastus.cloudapp.azure.com/backend/api/",
+    http_public_url: "https://nelab-ingress.eastus.cloudapp.azure.com/backend/",
     mqtt_url: "wss://nelab-ingress.eastus.cloudapp.azure.com/" + ":" + "32729" + "/mqtt",
     data_fetcher_http_url: "https://nelab-ingress.eastus.cloudapp.azure.com/",
     dashboardHeaders: new Headers()
