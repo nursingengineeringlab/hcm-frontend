32bb87adcf0518bcc6fe4ef873d273758253dab0
Shiyang Wang <shiyangwang@umass.edu>
2022-04-21 11:37:17 -0400

    some updates

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
     http_public_url: "https://nelab-ingress.eastus.cloudapp.azure.com/backend/",
-    mqtt_url: "wss://nelab-ingress.eastus.cloudapp.azure.com/" + ":" + "32729" + "/mqtt",
+    mqtt_url: "wss://nelab-ingress.eastus.cloudapp.azure.com" + ":" + "8084" + "/mqtt",
     data_fetcher_http_url: "https://nelab-ingress.eastus.cloudapp.azure.com/",
     dashboardHeaders: new Headers()
 };
