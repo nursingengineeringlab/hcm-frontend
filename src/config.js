e85e8f0d6477c3dd23c041a2043c80f6dddb02db
Jeff_ <4463820+lovejavaee@users.noreply.github.com>
2022-10-29 22:35:35 -0400

    RBAC

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
-    http_public_url: "https://nelab.ddns.umass.edu/backend/",
-    mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848/mqtt",
-    data_fetcher_http_url: "https://nelab.ddns.umass.edu/",
+    // http_public_url: "https://nelab.ddns.umass.edu/backend/",
+    http_public_url: "http://localhost:8000/backend/",
+    // mqtt_url: "ws://nelab.ddns.umass.edu" + ":" + "30848/mqtt",
+    mqtt_url: "ws://localhost" + ":" + "30848/mqtt",
+    //  data_fetcher_http_url: "https://nelab.ddns.umass.edu/",
+    data_fetcher_http_url: "https://localhost/",
     dashboardHeaders: new Headers()
 };
