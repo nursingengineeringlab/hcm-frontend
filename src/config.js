a3d71eaec1604dc0d4a5000ddb4e9fc36d96a5fa
Beizong Chen <beizongchen@umass.edu>
2022-11-24 04:50:34 +0000

    Settings for non-cloud setup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = global.config = {
     //most of those port does not need to be changed.
     http_public_url: "http://nelab.ddns.umass.edu/backend/",
-    mqtt_url: "ws://nelab.ddns.umass.edu/mqtt",
+    mqtt_url: "ws://nelab.ddns.umass.edu:8083/mqtt",
     data_fetcher_http_url: "http://nelab.ddns.umass.edu/datafetcher",
     dashboardHeaders: new Headers()
 };
